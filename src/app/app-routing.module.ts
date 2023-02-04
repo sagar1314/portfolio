96247b7822801eac3f8e6b221cdd9b37d7eb600b
jogmerkarsagar <jogmerkarsagar@gmail.com>
2023-02-04 18:09:28 +0530

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './pages/contact/contact.component';
-import { EducationComponent } from './pages/education/education.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ProjectComponent } from './pages/project/project.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { SkillsComponent } from './pages/skills/skills.component';
-import { WildcartComponent } from './wildcart/wildcart.component';
 
-const routes: Routes = [
-  {path:"",component:HomeComponent},
-  {path:"skills",component:SkillsComponent},
-  {path:"services",component:ServicesComponent},
-  {path:"edu",component:EducationComponent},
-  {path:"project",component:ProjectComponent},
-  {path:"contact",component:ContactComponent},
-  {path:"**",component:WildcartComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WildcartComponent } from '../wildcart/wildcart.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
+import { ServicesComponent } from './services/services.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"skills",component:SkillsComponent},
+  {path:"services",component:ServicesComponent},
+  {path:"edu",component:EducationComponent},
+  {path:"project",component:ProjectComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"**",component:WildcartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
